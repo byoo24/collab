379598d08e65680dc6466521586f2fdbc03ec5e9
byoo24 <byoo.dev@gmail.com>
2019-10-14 12:46:51 -0400

    added path to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const typeDefs = require('./graphql/graphql-types');
